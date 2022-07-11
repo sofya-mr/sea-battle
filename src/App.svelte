6187aa645a6b69ed46d12fd4d296b446bffa7113
Sofya Marozava <sofya.mr@gmail.com>
2022-07-11 22:01:07 +0300

    [update] in progress

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,12 @@
 	let damagedCells = 0;
 	let shipsLeft = 0;
 	let attempts = 0;
-	let text='something';
-	let unicornState=false;
+	let text = "Sea battle";
+	let unicornState = false;
+	let powerLevel=0;
+
+
+
 
 	const fieldSize = 6;
 	const cells = [];
@@ -37,7 +41,7 @@
 	function getCoords(i, obj) {
 		watchDog++;
 		if (watchDog > 1000) alert("bad things happen");
-	
+
 		let dir = directions[Math.floor(Math.random() * 2)];
 		if (
 			obj.x * dir.x > fieldSize - ships[i].shipLength ||
@@ -123,15 +127,19 @@
 
 	function checkCell(i) {
 		attempts += 1;
+		text="Missed...";
 
 		if (cells[i].ship) {
 			if (!cells[i].checked) {
 				cells[i].ship.cellDamage += 1;
+				text="Hit!"
 				damagedCells += 1;
+				powerLevel +=1;
 				if (cells[i].ship.cellDamage === cells[i].ship.shipLength) {
 					cells[i].ship.isSunk = true;
 					sunkShips += 1;
 					shipsLeft = ships.length - sunkShips;
+					text="You sunk a ship!"
 				}
 			}
 
@@ -139,46 +147,67 @@
 		}
 
 		cells[i].checked = true;
+		
+		
 	}
 
-//magic
+	//magic
 
-function magic(i) {
-	for (let ship of ships){
-	// ships.forEach((ship) => {
-		if (ship.isSunk==false){
-			ship.isSunk=true;
-			ship.damagedCells = ship.shipLength;
-			for(let i=0; i< ship.coords.length; i++) {
-				cells[ship.coords[i]].checked = true;
+	function magic(i) {
+
+		unicornState = !unicornState;
+		setTimeout(() => {
+			powerLevel=0;
+		}, 1000)
+
+		setTimeout(() => {
+			for (let ship of ships) {
+			// ships.forEach((ship) => {
+			if (ship.isSunk == false) {
+				ship.isSunk = 2;
+				sunkShips+=1;
+				text="Magic unicorn sunk a ship!"
+				shipsLeft = ships.length - sunkShips;
+				ship.damagedCells = ship.shipLength;
+				damagedCells+=ship.shipLength;
+				for (let i = 0; i < ship.coords.length; i++) {
+					cells[ship.coords[i]].checked = true;
+				}
+				
+				break;
 			}
-			break;
 		}
+
+		}, 2000)
+		
+		
 	}
 
-	unicornState=!unicornState;
-
+$: if (sunkShips==ships.length) {
+	text="Congratulations! You won."
+	
 }
-
-
 
 </script>
 
 <div>
-	<img src="https://www.clipartmax.com/png/small/206-2061211_unicorn-gif-transparent-background.png" 
-	alt="Unicorn Gif" class="image" class:fly={!unicornState}>
+	<img
+		src="./unicorn.png"
+		alt="Unicorn Gif"
+		class="image"
+		class:fly={!unicornState}
+	/>
 </div>
-
 
 <p>{text}</p>
 <div class="container" style="width:{fieldSize * 80}px">
 	{#each cells as cell, i}
 		<button
-			class:ship={cell.ship}
 			on:click={() => checkCell(i)}
 			class:checked={cell.checked}
 			class:damaged={cell.ship && cell.checked}
 			class:sunk={cell.ship && cell.ship.isSunk}
+			class:sunkByMagic={cell.ship && cell.ship.isSunk==2}
 		/>
 	{/each}
 </div>
@@ -194,17 +223,40 @@ function magic(i) {
 		</div>
 	</div>
 
+{#if sunkShips!==ships.length}
 	<div>
 		<div>The button will be activated when you gain enough power:</div>
 
 		<div class="scaleContainer">
-			<button class="magicButton" on:click={magic}>Let magic unicorns help</button>
-			<div class="scale closer{damagedCells < 5 ? damagedCells : 5}" />
+			<button class="magicButton" on:click={magic} disabled={powerLevel<5}>
+				Let magic unicorn help</button>
+			<div class="scale closer{powerLevel <= 5? powerLevel:5}" />
 		</div>
 	</div>
+{:else}
+
+<div class=scaleContainer>
+	<h2>You are the winner!</h2>
+	<button class='resettingButton' >
+		RESTART GAME
+	</button>
 </div>
 
+{/if}
+</div>
+
+
+
 <style>
+    h2{
+		margin-top:5px;
+		margin-bottom:16px;
+	}
+
+	p {
+		margin: 0px;
+	}
+
 	.container {
 		/* width:80%; */
 		display: flex;
@@ -214,15 +266,12 @@ function magic(i) {
 
 	.statsContainer {
 		padding: 0 20px;
-
 	}
-	.stats{
+	.stats {
 		white-space: nowrap;
 	}
 
-	.ship {
-		background-color: aqua;
-	}
+	
 
 	button {
 		margin-top: 0;
@@ -242,65 +291,102 @@ function magic(i) {
 		background-color: transparent;
 	}
 
-	.bottom{
+	.resettingButton {
+		width: 100%;
+		height: 40px;
+		border-radius: 5px;
+	}
+
+	.bottom {
 		display: flex;
-		width:480px;
-		margin-top:5px;
+		width: 480px;
+		margin-top: 5px;
 	}
 	.scale {
-		position:absolute;
-	    z-index:-1;
-		 top:0; 
-		 left:0; 
-		 width:5%; 
-		 height:40px; 
-		 background:rgb(250, 60, 60);
-		 border-radius: 5px;
+		position: absolute;
+		z-index: -1;
+		top: 0;
+		left: 0;
+		width: 5%;
+		height: 40px;
+		background: rgb(250, 60, 60);
+		border-radius: 5px;
 
 		/* transition: background-color 0.5s; */
 		transition: width 1s;
 	}
 
 	.closer1 {
-		width:20%; 
-		background:rgb(255, 145, 0);
+		width: 20%;
 	}
 
 	.closer2 {
-		width:40%;
-		background:rgb(97, 251, 102);
+		width: 40%;
+		background-color: rgb(97, 251, 102);
 	}
 
 	.closer3 {
-		width:60%;
-		background:rgb(0, 255, 247);
+		width: 60%;
+		background-color: rgb(0, 255, 247);
 	}
 
 	.closer4 {
-		width:80%;
-		background:rgb(117, 251, 217);	
+		width: 80%;
+		background-color: rgb(117, 251, 217);
 	}
 
 	.closer5 {
-		width:100%;
-		background:rgb(220, 248, 97);
-		color:yellow;
+		width: 100%;
+		background-color: rgb(220, 248, 97);
 	}
-	
+
+
+	.sunkByMagic {
+		background-color: #222;
+  animation-name: color;
+  animation-duration: 1s;
+  animation-iteration-count: 0.3;
+}
+
+@keyframes color {
+  0% {
+    background-color: #222;
+  }
+  20% {
+    background-color: #4285f4;
+  }
+  40% {
+    background-color: #222;
+  }
+  60% {
+    background-color: #4285f4;
+  } 
+  80% {
+    background-color: #222;
+  }
+  100% {
+    background-color: #4285f4;
+  }
+}
+
+
+
 	.image {
 		transition: left 3s;
 		position: absolute;
-		left:-1000px;
-		top:100px;
+		left: -1000px;
+		top: 100px;
 	}
 
-	.fly{
-		left:2000px;
+	.fly {
+		left: 2000px;
 	}
 
 	.scaleContainer {
 		margin-top: 5px;
-		position:relative;
+		position: relative;
+		text-align: center;
+   		width: 100%;
 	}
 
 	.checked {
